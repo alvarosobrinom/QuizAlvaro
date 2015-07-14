70e641f674096d6a92480c45450ad49961e0b500
alvarosobrinom <alvaro.sobrinom@gmail.com>
2015-07-14 16:01:09 +0200

    nuevos cambios

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 var express = requiere('express');
 var router = express.Router();
-/* GET home page. */
 router.get('/',function(req,res){
   res.render('index',{title:'Quiz'});
 });
 
 var quizController = require('../controllers/quiz_controller');
 
-// Definición de rutas de /quizes
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',quizController.answer);
 
